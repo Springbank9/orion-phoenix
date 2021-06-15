fa6df986ae538fbec1799bfe896cccf945f4df33
Springbank9 <isabellahay7@gmail.com>
2021-06-15 19:24:12 +0000

    different layout of timeline

--- js/script.js
+++ js/script.js
@@ -1,14 +1,63 @@
-(function() {
-  //Image Credit - https://medium.com/
+/*global $ */
+$(document).ready(function() {
 
-  //https://github.com/andreasstorm
-  $(window).scroll(function() {
-    var oVal;
-    oVal = $(window).scrollTop() / 240;
-    return $(".blur").css("opacity", oVal);
+  "use strict";
+
+  $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
+  //Checks if li has sub (ul) and adds class for toggle icon - just an UI
+
+  $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+  //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
+
+  $(".menu > ul").before("<a href=\"#\" class=\"menu-mobile\">Navigation</a>");
+
+  //Adds menu-mobile class (for mobile toggle menu) before the normal menu
+  //Mobile menu is hidden if width is more then 959px, but normal menu is displayed
+  //Normal menu is hidden if width is below 959px, and jquery adds mobile menu
+  //Done this way so it can be used with wordpress without any trouble
+
+  $(".menu > ul > li").hover(function(e) {
+    if ($(window).width() > 943) {
+      $(this).children("ul").stop(true, false).fadeToggle(150);
+      e.preventDefault();
+    }
   });
+  //If width is more than 943px dropdowns are displayed on hover
 
-}).call(this);
+  $(".menu > ul > li").click(function() {
+    if ($(window).width() <= 943) {
+      $(this).children("ul").fadeToggle(150);
+    }
+  });
+  //If width is less or equal to 943px dropdowns are displayed on click (thanks Aman Jain from stackoverflow)
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBRWdDO0VBQUE7OztFQUVoQyxDQUFBLENBQUUsTUFBRixDQUFTLENBQUMsTUFBVixDQUFpQixRQUFBLENBQUEsQ0FBQTtBQUNqQixRQUFBO0lBQUUsSUFBQSxHQUFRLENBQUEsQ0FBRSxNQUFGLENBQVMsQ0FBQyxTQUFWLENBQUEsQ0FBQSxHQUF3QjtXQUNoQyxDQUFBLENBQUUsT0FBRixDQUFVLENBQUMsR0FBWCxDQUFlLFNBQWYsRUFBMEIsSUFBMUI7RUFGZSxDQUFqQjtBQUZnQyIsInNvdXJjZXNDb250ZW50IjpbIiNJbWFnZSBDcmVkaXQgLSBodHRwczovL21lZGl1bS5jb20vXG5cbiNodHRwczovL2dpdGh1Yi5jb20vYW5kcmVhc3N0b3JtXG5cbiQod2luZG93KS5zY3JvbGwgLT5cbiAgb1ZhbCA9ICgkKHdpbmRvdykuc2Nyb2xsVG9wKCkgLyAyNDApXG4gICQoXCIuYmx1clwiKS5jc3MgXCJvcGFjaXR5XCIsIG9WYWwiXX0=
-//# sourceURL=coffeescript
+  $(".menu-mobile").click(function(e) {
+    $(".menu > ul").toggleClass('show-on-mobile');
+    e.preventDefault();
+  });
+  //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
+
+});
+
+$(document).ready(function(){
+  
+  setTimeout(function(){
+    
+    $("#name").removeClass("larger");
+
+    $("#name span").each(function(){
+      $(this).css("-webkit-transition-delay",$(this).data("delay")+"ms").css("transition-delay",$(this).data("delay")+"ms");
+      $(this).addClass("visible");
+    });
+    
+	}, 1000);    
+  
+});
+
+   
+                ScrollOut({
+                cssProps: {
+                    visibleY: true,
+                    viewportY: true
+                },
+                });
